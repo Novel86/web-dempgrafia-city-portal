eaddcdc059b465387b742a285d934cb846b40ee8
Roman Krets <romankrets@yandex.com>
2022-11-06 04:04:57 +0700

    страница администратора:
    -создал файлы;
    -подключил стили, добавил ссылку на индексе;
    -сверстал до таблицы включительно;
    -прописал js для выделения строк в таблице.

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,34 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+// Выделение строк в таблице администратора по клику мышкой
+
+let elements = Array.from(document.querySelectorAll('.tableNewRequestTable table tr')); // получаю элементы по которым кликать
+// console.log(elements);
+// console.log(elemActive.length);
+
+// для удаления существующих классов _active
+let removeActive = () => {
+
+	let elemActive = Array.from(document.querySelectorAll('._active')); // получаю элементы с классом _active
+	for (let elAc of elemActive) {
+		elAc.className = '';
+	}
+
+}
+
+for (let elem of elements) {
+
+	elem.onclick = () => {
+
+		removeActive();
+
+		elem.classList.toggle('_active');
+	}
+
+}
+
+
+
+
