b9b727851079595b8c78e6a36c5e198f0dd766be
Roman Krets <romankrets@yandex.com>
2022-11-18 00:55:58 +0700

    переподключил файл стилей с мин на обычный

--- php/_head.php
+++ php/_head.php
@@ -3,7 +3,7 @@
 	<meta charset="UTF-8">
 	<meta name="format-detection" content="telephone=no">
 	<!-- <style>body{opacity: 0;}</style> -->
-	<link rel="stylesheet" href="css/style.min.css?_v=20221116025507">
+	<link rel="stylesheet" href="css/style.css">
 	<link rel="icon" type="image/svg+xml" href="img/favicon.svg">
 	<link rel="icon" type="image/png" href="img/favicon.png">
 	<!-- <meta name="robots" content="noindex, nofollow"> -->
